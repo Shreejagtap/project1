8e87ec15b8c6e8d5a95298f765169d2f5283ff75
Shreejagtap <shreenath.jagtap.sj@gmail.com>
2024-10-23 21:29:58 +0530

    multer & cloudinary

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 
 dotenv.config({
   path: "./env",
